3d6c0e0a90298155b01ca414e74f6dc07a886d98
jhuhnke <jkhuhnke11@yahoo.com>
2023-09-07 07:18:07 -0500

    Loading screen wip

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,14 +1,25 @@
-import React from 'react'; 
-import '../stylesheets/Loading.css'
+import React, { useEffect, useState } from 'react';
+import '../stylesheets/Loading.css';
 
 const LoadingScreen = () => {
-    return (
-        <div className = 'container'>
-            <div className='loading'>
-                <h2 className='loadingtext'>Loading...</h2>
-            </div>
-        </div>
-    );
-}; 
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowMessage(true);
+    }, 30000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <div className='container'>
+      <div className='loading'>
+        <h2 className='loading-text'>Loading...</h2>
+        {showMessage && <div className='loading-message'>Please be patient, the blockchain is chunky</div>}
+      </div>
+    </div>
+  );
+};
 
 export default LoadingScreen;
